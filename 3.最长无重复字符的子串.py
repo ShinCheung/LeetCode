434714dbe5c810fb899dcb09a99de36125273927
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-01 17:34:41 +0800

    Update 3.最长无重复字符的子串.py

--- 3.最长无重复字符的子串.py
+++ 3.最长无重复字符的子串.py
@@ -15,14 +15,30 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        unique_chars = set([])
-        j = 0
-        n = len(s)
-        longest = 0
-        for i in range(n):
-            while j < n and s[j] not in unique_chars:
-                unique_chars.add(s[j])
-                j += 1
-            longest = max(longest, j - i)
-            unique_chars.remove(s[i])
-        return longest
+        # 开始坐标和最大长度都是0
+        start = maxlen = 0
+        # 设置一个空字典
+        dt = {}
+        # 写一个for循环，循环s的长度
+        for i in range(len(s)):
+            # 判断s[i]是否在字典当中并且开始坐标小于字典中对应s[i]的值,也就是s[i]中的值一定要在start后面
+            if s[i] in dt and start <= dt[s[i]]: 
+                # 如果满足上面的条件,start等于当前的字符串i位置的值+1
+                start = dt[s[i]] + 1
+            else:
+                # 否则的话，返回最大的长度,maxlen,或者当前的i-起始值+1
+                maxlen = max(maxlen,i - start + 1) 
+            # 把dt[s[i]]重置为i
+            dt[s[i]] = i
+        return maxlen
+    # def lengthOfLongestSubstring(self, s):
+    #     maxlen = 0
+    #     start = 0
+    #     dt = {}
+    #     for i,c in enumerate(s):
+    #         if c in dt:
+    #             start = max(start,dt[c]+1)
+    #         maxlen = max(maxlen,i-start+1)   
+    #         dt[c] = i
+    #     return maxlen
+print(Solution().lengthOfLongestSubstring('12345678980'))
