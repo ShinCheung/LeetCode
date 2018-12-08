723eae1404f1746fdc35437323fbdb6bb05c3fd0
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-08 17:28:08 +0800

    Update 101.对称二叉树.py

--- 101.对称二叉树.py
+++ 101.对称二叉树.py
@@ -1,13 +1,11 @@
 # 给定一个二叉树，检查它是否是镜像对称的
 # 例如，二叉树 [1,2,2,3,4,4,3] 是对称的
-
 #     1
 #    / \
 #   2   2
 #  / \ / \
 # 3  4 4  3
 # 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
-
 #     1
 #    / \
 #   2   2
@@ -36,4 +34,3 @@ class Solution:
             return l.val == r.val and self.digui(l.left, r.right) and self.digui(l.right, r.left)
         else:
             return not l and not r
-        
