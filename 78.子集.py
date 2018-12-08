e5973f4ad5bac9d8924c7d40002054028f52a215
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-08 15:14:21 +0800

    Add files via upload

---
+++ 78.子集.py
@@ -0,0 +1,33 @@
+# 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集
+# 说明：解集不能包含重复的子集
+# 示例:
+# 输入: nums = [1,2,3]
+# 输出:
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+class Solution:
+    def subsets(self, nums):
+        res = [[]]
+        for i in range(len(nums)):
+            for j in range(len(res)):
+                res.append(res[j] + [nums[i]])
+        return res
+    
+    def subsets2(self, nums):
+        res = [[]]
+        while nums:
+            x = nums.pop(0)
+            for i in res[:]:
+                res.append(i + [x])
+        return res
+
+print(Solution().subsets([1,2,3]))
+print(Solution().subsets2([1,2,3]))
