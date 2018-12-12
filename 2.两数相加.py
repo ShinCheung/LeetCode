f977b6ea8f33deeeda5f34592fe44484d11f3ddb
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-12 16:15:27 +0800

    Add files via upload

---
+++ 2.两数相加.py
@@ -0,0 +1,40 @@
+# 给出两个 非空 的链表用来表示两个非负的整数。
+# 其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+# 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+# 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+# 示例：
+# 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+# 输出：7 -> 0 -> 8
+# 原因：342 + 465 = 807
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        p = head
+        carry = 0
+        while True:
+            if l1 != None:
+                carry += l1.val
+                l1 = l1.next
+            if l2 != None:
+                carry += l2.val
+                l2 = l2.next
+            p.val = carry % 10
+            carry //= 10
+            # 运算未结束新建一个节点用于储存答案，否则退出循环
+            if l1 != None or l2 != None or carry != 0:
+                p.next = ListNode(0)
+                p = p.next
+            else: 
+                break
+        return head
