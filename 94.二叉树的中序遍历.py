4407495f94d2bc1785113abdd3c06820326bd2cf
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-15 16:59:23 +0800

    Add files via upload

---
+++ 94.二叉树的中序遍历.py
@@ -0,0 +1,32 @@
+# 给定一个二叉树，返回它的中序 遍历。
+# 示例:
+# 输入: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+# 输出: [1,3,2]
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        res = []
+        if root.left:
+            res += self.inorderTraversal(root.left)
+        if root.val:
+            res.append(root.val)
+        if root.right:
+            res += self.inorderTraversal(root.right)
+        return res
