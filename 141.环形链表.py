1927e646760684a95eb532a3d965e7542f7caf8c
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-16 21:29:57 +0800

    Update 141.环形链表.py

--- 141.环形链表.py
+++ 141.环形链表.py
@@ -4,7 +4,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+# 快慢指针找环，快指针一次走两步，慢指针一次走一步，两个指针碰撞时（meet），说明找到环
 class Solution(object):
     def hasCycle(self, head):
         """
