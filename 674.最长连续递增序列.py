b7002212a29b9f0759543cf006223855c0389903
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-23 18:41:57 +0800

    Add files via upload

---
+++ 674.最长连续递增序列.py
@@ -0,0 +1,26 @@
+# 给定一个未经排序的整数数组，找到最长且连续的的递增序列。
+# 示例 1:
+# 输入: [1,3,5,4,7]
+# 输出: 3
+# 解释: 最长连续递增序列是 [1,3,5], 长度为3。
+# 尽管 [1,3,5,7] 也是升序的子序列, 但它不是连续的，因为5和7在原数组里被4隔开。 
+# 示例 2:
+# 输入: [2,2,2,2,2]
+# 输出: 1
+# 解释: 最长连续递增序列是 [2], 长度为1。
+class Solution:
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        res = tmp = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                tmp += 1
+            else:
+                tmp = 1
+            res = max(res, tmp)
+        return res
